10d6223dd56200bc693a7c773053ae946f06727a
khi <khi805@naver.com>
2017-11-16 17:59:13 +0900

    test

--- Ch10_Collections/src/test/HashTableExam.java
+++ Ch10_Collections/src/test/HashTableExam.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class HashTableExam {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		Map<String,String> map = new Hashtable<>();
 		//1.회원가입 2.로그인 3.종료
 		//1.회원가입 - scanner로 입력받아서 저장
 		// 반복 저장 종료.
@@ -17,49 +18,53 @@ public class HashTableExam {
 		while(true) {
 			System.out.println("1.회원가입|2.로그인|3.종료");
 			int selectNo = sc.nextInt();
+			boolean isContinue = true;
 			switch(selectNo) {
 			case 1 : 
-				System.out.println("[회원가입]");
-				System.out.print("아이디 입력>");
-				String id = sc.next();
-				System.out.print("비밀번호 입력>");
-				String password = sc.next();
-				if()
-			case 2 :
-			case 3 :
-			}
-		}
-		Map<String,String> map = new Hashtable<>();
-		
-		map.put("spring", "1234");
-		map.put("summer", "1234");
-		map.put("fall", "1234");
-		map.put("winter", "1234");
-		
-		
-		int cnt = 0;
-		while(true) {
-			System.out.println("아이디와 패스워드를 입력하세요");
-			System.out.print("아이디: ");
-			String id = sc.next();
-			
-			System.out.print("패스워드: ");
-			String password = sc.next();
-	
-			if(map.containsKey(id)) {
-				if(map.get(id).equals(password)) {
-					System.out.println("로그인 완료");
-					break;
-				} else {
-					if(cnt==5) {
-						System.out.println("비밀번호 5회 입력 실패로 종료합니다.");
-						break;
+				while(isContinue) {
+					System.out.println("[회원가입]");
+					System.out.print("아이디 입력>");
+					String id = sc.next();
+					System.out.print("비밀번호 입력>");
+					String password = sc.next();
+					if(map.containsKey(id)) {
+						System.out.println("같은 아이디가 이미 존재합니다.");
+						continue;
+					}else {
+						map.put(id,password);
+						System.out.println("회원가입 완료!");
+						isContinue = false;
 					}
-					System.out.println("비밀번호가 일치하지 않습니다.");
-					cnt++;
 				}
-			} else {
-				System.out.println("입력한 아이디가 없습니다.");
+				continue;
+			case 2 :
+				int cnt = 0;
+				while(true) {
+					System.out.println("아이디와 패스워드를 입력하세요");
+					System.out.print("아이디: ");
+					String id = sc.next();
+					
+					System.out.print("패스워드: ");
+					String password = sc.next();
+			
+					if(map.containsKey(id)) {
+						if(map.get(id).equals(password)) {
+							System.out.println("로그인 완료");
+							break;
+						} else {
+							System.out.println("비밀번호가 일치하지 않습니다.");
+							cnt++;
+							if(cnt==5) {
+								System.out.println("비밀번호 5회 입력 실패로 종료합니다.");
+								break;
+							}
+						}
+					} else {
+						System.out.println("입력한 아이디가 없습니다.");
+					}
+				}
+			case 3 :
+				return;
 			}
 		}
 		
